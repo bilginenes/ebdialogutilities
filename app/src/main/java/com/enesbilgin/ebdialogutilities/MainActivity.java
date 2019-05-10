2f928874495a722d3457e4efe610d7c6f74b6e69
Enes Bilgin <enesbilgin04@gmail.com>
2019-05-10 18:19:18 +0300

    Main app improved.

--- app/src/main/java/com/enesbilgin/ebdialogutilities/MainActivity.java
+++ app/src/main/java/com/enesbilgin/ebdialogutilities/MainActivity.java
@@ -18,7 +18,7 @@ import com.enesbilgin.ebdialogutilities.Utilities.EBDialogUtilities;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button button1, button2, button3, button4;
+    private Button button1, button2, button3, button4, button5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
         button2 = findViewById(R.id.button2);
         button3 = findViewById(R.id.button3);
         button4= findViewById(R.id.button4);
+        button5= findViewById(R.id.button5);
 
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,11 +41,35 @@ public class MainActivity extends AppCompatActivity {
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                EBDialogUtilities.showErrorBox(MainActivity.this,"This is a error message.");
+                EBDialogUtilities.showNotSupportedInfoBox(MainActivity.this);
             }
         });
 
         button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EBDialogUtilities.showErrorBox(MainActivity.this,"This is a error message.");
+            }
+        });
+
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EBDialogUtilities.showConfirmBox(MainActivity.this, "Confirm Process", "Do you want to proceed the operation?", new CompletionListener() {
+                    @Override
+                    public void onSuccess() {
+                        Toast.makeText(MainActivity.this,"onSuccess", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        Toast.makeText(MainActivity.this,"onCancel", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+
+        button5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 EBDialogUtilities.showVoteBox(MainActivity.this, getString(R.string.app_name), new VoteChoiceListener() {
@@ -61,23 +86,6 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onLater() {
                         Toast.makeText(MainActivity.this,"onLater", Toast.LENGTH_SHORT).show();
-                    }
-                });
-            }
-        });
-
-        button4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                EBDialogUtilities.showConfirmBox(MainActivity.this, "Confirm Process", "Do you want to proceed the operation?", new CompletionListener() {
-                    @Override
-                    public void onSuccess() {
-                        Toast.makeText(MainActivity.this,"onSuccess", Toast.LENGTH_SHORT).show();
-                    }
-
-                    @Override
-                    public void onCancel() {
-                        Toast.makeText(MainActivity.this,"onCancel", Toast.LENGTH_SHORT).show();
                     }
                 });
             }
